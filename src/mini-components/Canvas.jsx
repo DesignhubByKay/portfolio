4ad3fbede19c8763ca8c1293e41e715c0cd1dcc6
DesignhubByKay <designhub.kay@gmail.com>
2026-09-26 17:08:39 +0530

    Landing Issue

--- src/mini-components/Canvas.jsx
+++ src/mini-components/Canvas.jsx
@@ -13,7 +13,7 @@ export default function Canvas({ setTl }) {
     useGSAP(() => {
         const canvas = canvasEl.current;
         if (!canvas) return;
-
+        ScrollTrigger.normalizeScroll(true);
         let resizeHandler;
         canvas.width = window.innerWidth;
         canvas.height = window.innerHeight;
@@ -71,11 +71,12 @@ export default function Canvas({ setTl }) {
             const landingTl = gsap.timeline({
                 scrollTrigger: {
                     trigger: '.landing',
-                    start: 'top top',
+                    start: 'top -1px',
                     end: '+=1500',
                     scrub: 1,
                     pin: true,
-                    pinSpacing: true
+                    pinSpacing: true,
+                    anticipatePin: 1
                 }
             });
             setTl(landingTl);
